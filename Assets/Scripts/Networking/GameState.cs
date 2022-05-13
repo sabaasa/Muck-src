f44d44518d4ad30e9fe614f2c23c2ef57342aa9e
Lyssa <75037904+sabaasa@users.noreply.github.com>
2022-05-13 08:38:00 -0700

    Add files via upload

---
+++ Assets/Scripts/Networking/GameState.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Assets.Scripts.Networking
+{
+	public enum GameState
+	{
+		Lobby,
+		Playing,
+		Discuss
+	}
+}
---
+++ DebugStuff/ConsoleToGUI.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace DebugStuff
+{
+	public class ConsoleToGUI : MonoBehaviour
+	{
+		private void Awake()
+		{
+			if (ConsoleToGUI.Instance)
+			{
+				Object.Destroy(base.gameObject);
+				return;
+			}
+			ConsoleToGUI.Instance = this;
+			Object.DontDestroyOnLoad(base.gameObject);
+		}
+
+		private void OnEnable()
+		{
+			Application.logMessageReceived += this.Log;
+		}
+
+		private void OnDisable()
+		{
+			Application.logMessageReceived -= this.Log;
+		}
+
+		public void Log(string logString, string stackTrace, LogType type)
+		{
+			this.output = logString;
+			this.stack = stackTrace;
+			ConsoleToGUI.myLog = this.output + "\n" + ConsoleToGUI.myLog;
+			if (ConsoleToGUI.myLog.Length > 300)
+			{
+				ConsoleToGUI.myLog = ConsoleToGUI.myLog.Substring(0, 200);
+			}
+			this.debugText.text = ConsoleToGUI.myLog;
+		}
+
+		private void OnGUI()
+		{
+		}
+
+		public TextMeshProUGUI debugText;
+
+		private static string myLog = "";
+
+		private string output;
+
+		private string stack;
+
+		public static ConsoleToGUI Instance;
+	}
+}
